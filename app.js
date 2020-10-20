360ce1a7a680f80d4ab212e55c15d23b5ea6a21f
Fernando Lopez <flopez1018@gmail.com>
2020-10-20 15:20:49 -0500

    wireframing first page

--- app.js
+++ app.js
@@ -30,31 +30,40 @@ const store = {
 //FIRST PAGE STUFF
 //this render function takes in the parameter of quizstarted from store if false it will send to startPage if true it will send to question page function
 function render(){
-
+  if(store.quizStarted === false){
+    $("main").html(startPage);
+  }
 }
-
-
 //this function is going to have a div template to start out the app and a button with id="start" the button will begin the app 
 function startPage(){
-
+  let startPage = 
+    `
+      <div class="card">
+        <h2>Welcome to our quiz</h2>
+        <p>It's going to be fun and easy</p>
+        <button id="start"></button> 
+      </div>
+    `;
+  return startPage;
 }
 
 //begins from startPage function once the user clicks on the start button and sends to main function to render html 
 function handleStartQuiz(){
 
 }
+
 //END OF FIRST PAGE STUFF
 //SECOND PAGE AND BEYOND STUFF
 // in this function it will render the html template for the questions and multiple forms
 function questionPage(){
-  
+
 }
 
 
 //END OF SECOND PAGE AND BEYOND STUFF
 //this function is going to render all of the stubs
 function main(){
-
+    render();
 }
 
 $(main);
